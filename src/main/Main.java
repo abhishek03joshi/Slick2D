374dba916af1bfea34a671aa8afd8e1222364920
abhishek03joshi <abhishek03joshi@gmail.com>
2018-08-22 22:32:35 -0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Main extends StateBasedGame{
+
+	public Main(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		AppGameContainer appgc;
+		
+		appgc = new AppGameContainer();
+		
+		
+	}
+
+}
